c2f465be6267fa259a339245918d6a4bca5cccc8
ChandelVatsal <68132483+ChandelVatsal@users.noreply.github.com>
2024-06-19 17:49:27 -0700

    Arrays and Hashing

--- Arrays Hashing/longestConSeq.py
+++ Arrays Hashing/longestConSeq.py
@@ -1,2 +1,16 @@
 def longestConsecutive(nums):
-    pass
+    temp = set(nums)
+    longest = 0
+    
+    for numb in temp:
+        length = 0
+        
+        while numb - length in temp:
+            length += 1
+        
+        longest = max(longest, length)
+    
+    return longest
+
+
+print(longestConsecutive([0,3,2,5,4,6,1,1]))
